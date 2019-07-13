4212055638df698e271f0eb9c334cc057e7891f7
Ethan Nguyen <ethannguyen@gmail.com>
2019-07-13 14:41:19 +0700

    Add more param in text

--- app/src/main/java/com/example/demomvvm/features/litho/helloworld/LithoMainActivity.kt
+++ app/src/main/java/com/example/demomvvm/features/litho/helloworld/LithoMainActivity.kt
@@ -2,6 +2,7 @@ package com.example.demomvvm.features.litho.helloworld
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.demomvvm.R
 import com.facebook.litho.ComponentContext
 import com.facebook.litho.LithoView
 
@@ -9,6 +10,9 @@ class LithoMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(LithoView.create(this, RootComponentSpec.onCreateLayout(ComponentContext(this))))
+        setContentView(
+            LithoView.create(
+                this,
+                RootComponentSpec.onCreateLayout(ComponentContext(this), getString(R.string.app_name))))
     }
 }
--- app/src/main/java/com/example/demomvvm/features/litho/helloworld/RootComponentSpec.kt
+++ app/src/main/java/com/example/demomvvm/features/litho/helloworld/RootComponentSpec.kt
@@ -1,18 +1,27 @@
 package com.example.demomvvm.features.litho.helloworld
 
 import androidx.annotation.LayoutRes
+import com.example.demomvvm.R
 import com.facebook.litho.Component
 import com.facebook.litho.ComponentContext
 import com.facebook.litho.annotations.LayoutSpec
 import com.facebook.litho.annotations.OnCreateLayout
+import com.facebook.litho.annotations.Prop
 import com.facebook.litho.widget.Text
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 @LayoutSpec
 object RootComponentSpec {
 
     @OnCreateLayout
-    fun onCreateLayout(c: ComponentContext): Component {
-        return Text.create(c).textSizeSp(20f).text("Hello World").build()
+    fun onCreateLayout(c: ComponentContext,
+                       @Prop
+                       content: String): Component {
+        return Text
+            .create(c)
+            .textSizeSp(c.applicationContext.resources.getDimension(R.dimen.text_size))
+            .text(content)
+            .build()
     }
 
 }
